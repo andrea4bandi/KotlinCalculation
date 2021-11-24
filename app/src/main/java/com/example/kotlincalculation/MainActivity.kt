0d6287c237e1bf19e475765ab4f9d38e52b0accb
andrea4bandi <kunandi2gmail.com>
2021-11-24 21:36:45 +0100

    test2

--- app/src/main/java/com/example/kotlincalculation/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculation/MainActivity.kt
@@ -6,49 +6,60 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var num1: Int? = null
+    var num2: Int? = null
+    var res: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun sum(view: View){
-        val a= number1.text.toString().toInt()
-        val b= number2.text.toString().toInt()
+    fun sum(view: View) {
+        num1 = number1.text.toString().toIntOrNull()
+        num2 = number2.text.toString().toInt()
 
-        val result= a+b
+        if (num1 == null || num2 == null) {
 
-        textView.text="Result: " + result
+            textView.text = "Error"
 
-    }
+        } else {
+            res = num1!! + num2!!
+            textView.text = res.toString()
 
-    fun sub(view: View){
-
-        val a= number1.text.toString().toInt()
-        val b= number2.text.toString().toInt()
-
-        val result= a-b
-
-        textView.text="Result: " + result
+        }
 
     }
 
-    fun multiply(view: View){
-        val a= number1.text.toString().toInt()
-        val b= number2.text.toString().toInt()
+    fun sub(view: View) {
 
-        val result= a*b
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
 
-        textView.text="Result: " + result
+        val result = a - b
+
+        textView.text = "Result: " + result
 
     }
 
-    fun div(view: View){
-        val a= number1.text.toString().toInt()
-        val b= number2.text.toString().toInt()
+    fun multiply(view: View) {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
 
-        val result= a/b
+        val result = a * b
 
-        textView.text="Result: " + result
+        textView.text = "Result: " + result
+
+    }
+
+    fun div(view: View) {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
+
+        val result = a / b
+
+        textView.text = "Result: " + result
 
 
     }
